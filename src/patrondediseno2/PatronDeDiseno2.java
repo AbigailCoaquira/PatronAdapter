c3c0921eeca9b007a6740fc678d6681921e0fd34
AbigailCoaquira <coaquiraabigail4@gmail.com>
2022-11-25 06:05:10 -0500

    Subiendo_archivos

---
+++ src/patrondediseno2/PatronDeDiseno2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patrondediseno2;
+
+/**
+ *
+ * @author HP
+ */
+public class PatronDeDiseno2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
